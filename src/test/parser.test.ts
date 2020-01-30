b5ff8d585f592cecd0355b0f018d33768fe692ca
wulunyi <wulunyi@meituan.com>
2020-01-31 05:37:53 +0800

    style(master): 完善测试用例

--- src/test/parser.test.ts
+++ src/test/parser.test.ts
@@ -1,15 +1,59 @@
 import { parse } from '../parser';
 
 describe('Test Parser', () => {
-    it('Test Object', () => {
-        const ast = parse(`{}`);
+    it('Test ', () => {
+        const ast = parse(`{a:12,/**1*/}`);
 
         expect(ast).toEqual({
             type: 'Program',
             body: [
                 {
                     type: 'ObjectPattern',
-                    children: [],
+                    children: [
+                        {
+                            key: {
+                                loc: {
+                                    end: {
+                                        column: 2,
+                                        line: 1,
+                                    },
+                                    start: {
+                                        column: 2,
+                                        line: 1,
+                                    },
+                                },
+                                raw: 'a',
+                                type: 'Identifier',
+                                value: 'a',
+                            },
+                            loc: {
+                                end: {
+                                    column: 5,
+                                    line: 1,
+                                },
+                                start: {
+                                    column: 2,
+                                    line: 1,
+                                },
+                            },
+                            type: 'ObjectProperty',
+                            value: {
+                                loc: {
+                                    end: {
+                                        column: 5,
+                                        line: 1,
+                                    },
+                                    start: {
+                                        column: 4,
+                                        line: 1,
+                                    },
+                                },
+                                raw: '12',
+                                type: 'Literal',
+                                value: 12,
+                            },
+                        },
+                    ],
                     loc: {
                         start: {
                             line: 1,
@@ -17,14 +61,30 @@ describe('Test Parser', () => {
                         },
                         end: {
                             line: 1,
-                            column: 2,
+                            column: 13,
                         },
                     },
                 },
             ],
             start: 1,
-            end: 2,
-            comments: [],
+            end: 13,
+            comments: [
+                {
+                    loc: {
+                        end: {
+                            column: 12,
+                            line: 1,
+                        },
+                        start: {
+                            column: 7,
+                            line: 1,
+                        },
+                    },
+                    raw: '/**1*/',
+                    type: 'BlockComment',
+                    value: ['1'],
+                },
+            ],
             loc: {
                 start: {
                     line: 1,
@@ -32,7 +92,7 @@ describe('Test Parser', () => {
                 },
                 end: {
                     line: 1,
-                    column: 2,
+                    column: 13,
                 },
             },
         });
